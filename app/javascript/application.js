6a4e32024bb37e08fcc788a639f863bc1f7d2026
reddevx <juan.devx@gmail.com>
2024-05-26 15:13:56 -0300

    adding header and footer

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+
+//= require popper
+//= require bootstrap
+//= require rails-ujs
+//= require activestorage
+//= require_tree .
+
 import "@hotwired/turbo-rails"
 import "controllers"
