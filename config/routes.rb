6dc4f4fe3bbc702b92f67d53846e569cb442e681
reddevx <juan.devx@gmail.com>
2024-05-26 16:05:08 -0300

    adding items for a model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "static_pages#index"
   get 'sobre', to: "static_pages#sobre"
   get 'contato', to: "static_pages#sobre"
-
+  resources :users, only: [:show, :new, :create, :edit, :update]
   resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
+
 end
